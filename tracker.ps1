db82373f6a4064bfea04fd0e889ffa1633d7a456
Losson Sébastien <30404478+lseb1978@users.noreply.github.com>
2022-08-11 02:05:10 +0200

    Update tracker.ps1

--- tracker.ps1
+++ tracker.ps1
@@ -11,3 +11,16 @@ $data = Invoke-RestMethod $url -Method GET
 
 
 $data
+
+$data.latitude = $data.latitude -replace ',','.' 
+$data.longitude = $data.longitude -replace ',','.' 
+
+
+$googlemapsurl = 'https://maps.google.com/maps?q='+$data.latitude+','+$data.longitude+'&z=1'
+
+
+start microsoft-edge:$googlemapsurl
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('Google - Microsoft Edge')
+Sleep 2
+$wshell.SendKeys('{F11}')
